536fa3b9b6f9bf6a10e107619f10c60e81114cef
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-11-11 13:06:40 +0530

    Complete styling of Input CV Form

--- src/App.jsx
+++ src/App.jsx
@@ -18,30 +18,33 @@ function App() {
       </header>
       <main>
         <section className="formInp">
-          <label htmlFor="fName">First Name:</label>
-          <input value={fName} type="text" maxLength={12} id="fName" onChange={(e) => setfName(e.target.value)} required></input>
-
-          <label htmlFor="lName">Last Name:</label>
-          <input value={lName} type="text" maxLength={12} id="lName" onChange={(e) => setlName(e.target.value)} required></input>
-
-          <label htmlFor="age">Age:</label>
-          <input value={age} type="number" max={100} id="age" onChange={(e) => setAge(e.target.value)} required></input>
+          <h2>Introduction</h2>
+          <section className="intro">
+            <label htmlFor="fName">First Name:</label>
+            <input value={fName} type="text" maxLength={12} id="fName" onChange={(e) => setfName(e.target.value)} required></input>
+            <label htmlFor="lName">Last Name:</label>
+            <input value={lName} type="text" maxLength={12} id="lName" onChange={(e) => setlName(e.target.value)} required></input>
+            <label htmlFor="age">Age:</label>
+            <input value={age} type="number" max={100} id="age" onChange={(e) => setAge(e.target.value)} required></input>
+          </section>
 
 
           <h2>Education</h2>
-          <label htmlFor="collegN">University name:</label>
-          <input value={uName} type="text" maxLength={12} id="collegN" onChange={(e) => setUName(e.target.value)} required></input>
+          <section className="educ">
+            <label htmlFor="collegN">University name:</label>
+            <input value={uName} type="text" maxLength={12} id="collegN" onChange={(e) => setUName(e.target.value)} required></input>
+            <label htmlFor="degreeN">Degree name:</label>
+            <input value={dName} type="text" maxLength={12} id="degreeN" onChange={(e) => setDName(e.target.value)} required></input>
+            <label htmlFor="gradPerc">Graduation %:</label>
+            <input value={perc} type="number" max={100} id="gradPerc" onChange={(e) => setPerc(e.target.value)} required></input>
+          </section>
 
-          <label htmlFor="degreeN">Degree name:</label>
-          <input value={dName} type="text" maxLength={12} id="degreeN" onChange={(e) => setDName(e.target.value)} required></input>
 
-          <label htmlFor="gradPerc">Graduation %:</label>
-          <input value={perc} type="number" max={100} id="gradPerc" onChange={(e) => setPerc(e.target.value)} required></input>
-
-
-          <h3>Skills</h3>
-          <label htmlFor="skills">Skills :</label>
-          <input value={skills} type="text" id="skills" onChange={(e) => setSkills(e.target.value)}></input>
+          <h2>Skills</h2>
+          <section className="skill">
+            <label htmlFor="skills">Skills :</label>
+            <input value={skills} type="text" id="skills" onChange={(e) => setSkills(e.target.value)}></input>
+          </section>
         </section>
         <section className="formDisp">
           <Display fName={fName} lName={lName} age={age} uName={uName} dName={dName} perc={perc} skills={skills}/>

2bc2fc2c541fdb0bbdf105baa4ce61fb8965206c
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-11-15 11:46:05 +0530

    Finish styling of experiences display

--- src/Components/experiences.jsx
+++ src/Components/experiences.jsx
@@ -1,20 +1,32 @@
+import '/home/fudo/repos/CV-App/src/Styles/experiences.css'
 export default function Experiences({
     cName,
     rName,
     dF,
     dT
-}){
+}) {
     let nodeList = []
     for (let i = 0; i < cName.length; i++) {
         nodeList.push(
-            <section className="company">
-                <p>In the company <strong>{cName[i]}</strong> while working on role of <strong>{rName[i]}</strong> from <strong>{dF[i]}</strong> to <strong>{dT[i]}</strong></p>
+            <section className="company" key={i}>
+                <div className="labels">
+                    <h3>Company Name</h3>
+                    <h3>Role Name</h3>
+                    <h3>From</h3>
+                    <h3>To</h3>
+                </div>
+                <div className="information">
+                    <p>{cName[i]}</p>
+                    <p>{rName[i]}</p>
+                    <p>{dF[i]}</p>
+                    <p>{dT[i]}</p>
+                </div>
             </section>
         )
     }
     return (
         <>
-        {nodeList.map((node) => node)}
+            {nodeList.map((node) => node)}
         </>
     )
 }
